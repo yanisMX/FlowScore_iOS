880d138f49874541bbb77d6f91347219839d9a88
Yanis Mohamed <yanismxd@gmail.com>
2024-01-17 15:31:43 +0100

    Initial Commit

---
+++ FlowScore/FlowScoreApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlowScoreApp.swift
+//  FlowScore
+//
+//  Created by Yanis Mohamed on 17/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct FlowScoreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
